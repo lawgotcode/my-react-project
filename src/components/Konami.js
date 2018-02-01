63b3331e779b47a9bf3a5a8c4ea29e31339cab76
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-02-01 16:31:06 -0600

    new edit

--- src/components/Konami.js
+++ src/components/Konami.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
-
+import Mousetrap from 'mousetrap';
 
 class Konami extends Component {
+	constructor () {
+
+	}
+
+	componentDidMount() {
+		Mousetrap.bind([`1 2 3 4`, `up up down down left right left right b a`] this.popUp);
+
+	}
+
+	componentWillUnmount() {
+        Mousetrap.unbind([`1 2 3 4`, `up up down down left right left right b a`]);
+	}
+
+	popUp () {
+		alert('The Konami code happened!');
+	}
   render() {
     return (
       <div>Hello this is Konami</div>
